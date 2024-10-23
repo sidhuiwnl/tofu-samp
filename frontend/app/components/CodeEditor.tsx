76ad9ac98963c5a8db8037ea182f93a3ed0056d4
Sidharth <sidharthinfernal@gmail.com>
2024-10-23 08:17:41 +0530

    backend url changes

--- frontend/app/components/CodeEditor.tsx
+++ frontend/app/components/CodeEditor.tsx
@@ -12,7 +12,7 @@ export default function TheEditorComponent(){
     const editorRef = useRef();
     const [value,setValue] = useState<string | undefined>("");
     const [language,setLanguage] = useState("javascript");
-    const { socket, isConnected, sendMessage } = useWebsocket("https://tofu-9j1l.onrender.com");
+    const { socket, isConnected, sendMessage } = useWebsocket("https://tofu-samp.onrender.com");
 
     
     useEffect(() =>{
--- frontend/app/components/Output.tsx
+++ frontend/app/components/Output.tsx
@@ -12,7 +12,7 @@ export default function Output({editorRef,language} : {editorRef: any,language :
    const [isLoading,setIsLoading] = useState(false);
    const [isError,setIsError] = useState(false);
 
-   const { socket,isConnected,sendMessage} = useWebsocket("https://tofu-9j1l.onrender.com");
+   const { socket,isConnected,sendMessage} = useWebsocket("https://tofu-samp.onrender.com");
 
    useEffect(() => {
     if (socket) {
