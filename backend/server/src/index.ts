ef5e16a32304c791f5367504eabbf5339495cfc2
Sidharth <sidharthinfernal@gmail.com>
2024-11-14 19:42:16 +0530

    init

--- backend/server/src/index.ts
+++ backend/server/src/index.ts
@@ -1,14 +1,13 @@
-import WebSocket from "ws";
+import WebSocket, { WebSocketServer } from "ws";
 
-const wss = new WebSocket.Server({ port: 8000 });
+const wss = new WebSocketServer({ port: 8000 });
 
-wss.on("connection", (ws) => {
-  console.log("a client connected");
+wss.on("connection", (ws: WebSocket) => {
+  console.log("A client connected");
 
   ws.on("message", (message: WebSocket.RawData) => {
     const data = JSON.parse(message.toString());
 
-    
     wss.clients.forEach((client) => {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(JSON.stringify(data));
